fc67d7a8356e5725ab83b47ff1325b1894992aab
zemeister2332 <zemeister2332@gmail.com>
2020-07-07 00:30:14 +0500

    Start

---
+++ helpers/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+module.exports = () => {
+    mongoose.connect(
+        process.env.DB_STRING,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }
+
+    );
+
+    mongoose.connection.on('open', () => {
+        console.log("MongoDB Is Online!");
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log("MongoDB Is Not Connected!!!",err);
+    });
+}
